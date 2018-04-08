32b6a9e023078ad09bdf7ea459ccbc2c06373533
Jurikay <jurek.baumann@gmail.com>
2018-04-08 05:01:38 +0200

    Reimplement paint method in custom delegate

--- app/table_items.py
+++ app/table_items.py
@@ -1,12 +1,15 @@
-from PyQt5.QtWidgets import (QWidget, QApplication, QTableWidget,
-                             QTableWidgetItem, QVBoxLayout, QStyledItemDelegate)
+from PyQt5.QtWidgets import (QApplication, QStyledItemDelegate, QStyleOptionViewItem)
 from PyQt5.QtWidgets import QStyle
-from PyQt5.QtGui import QFont, QColor
+from PyQt5.QtGui import QTextDocument, QAbstractTextDocumentLayout, QPalette
 from app.colors import colors
+from PyQt5.QtCore import QSize
+from PyQt5 import Qt
+
 
 class CoinDelegate(QStyledItemDelegate):
     """
     TAKEN FROM https://stackoverflow.com/questions/43300034/qt-itemdelegate-displaytext-with-a-qtablewidget-how-can-i-access-the-table-in/43300126#43300126
+    AND https://stackoverflow.com/questions/41760474/pyqt-qstyleditemdelegate-word-wrap-and-html/43417837
     The following methods are subclassed to replace display and editor of the
     QTableWidget.
     """
@@ -19,28 +22,39 @@ class CoinDelegate(QStyledItemDelegate):
 
     def initStyleOption(self, option, index):
         """
-        Initialize `option` with the values using the `index` index. When the 
-        item (0,1) is processed, it is styled especially. All other items are 
+        Initialize `option` with the values using the `index` index. When the
+        item (0,1) is processed, it is styled especially. All other items are
         passed to the original `initStyleOption()` which then calls `displayText()`.
         """
         # print("delegate stuff")
+        # print(self.parent.name())
         if index.column() == 1:
-            option.text = str(index.data()) + " / BTC"
+            option.text = "<span style='color:#cdcdcd;'>" + str(index.data()) + " / BTC</span>"
 
-        elif index.column() == 2 or index.column() == 4:
+        elif index.column() == 2:
             # orig = str(index.data())
-            option.text = str(index.data()) + " BTC"
+            option.text = "<span style='color:#cdcdcd;'>" + str(index.data()) + " BTC</span>"
 
         elif index.column() == 3:
             percent_value = float(index.data())
-            option.color = colors.color_green
             if percent_value > 0:
                 operator = "+"
-                
+                color = "#94c940"
+
+            elif percent_value == 0.00:
+                operator = ""
+                color = "#999"
+
             else:
                 operator = ""
+                color = "#ff007a"
 
-            option.text = operator + "{0:.2f}".format(percent_value) + "%"
+            option.text = "<span style='color:" + color + ";'>" + operator + "{0:.2f}".format(percent_value) + "%"
+            option.textAlignmentRole = Qt.Qt.AlignRight
+
+
+        elif index.column() == 4:
+            option.text = "<span style='color:#cdcdcd;'>" + "{0:,.1f}".format(float(index.data())) + " BTC</span>"
 
         else:
             # print(index.data())
@@ -58,3 +72,42 @@ class CoinDelegate(QStyledItemDelegate):
         """
         data = text  # .toString() # Python 2: need to convert to "normal" string
         return "{0:>{1}}".format(data, 4)
+
+
+    def paint(self, painter, option, index):
+        """Reimplement paint function to allow html rendering within tableWidgets."""
+        options = QStyleOptionViewItem(option)
+        self.initStyleOption(options, index)
+
+        style = QApplication.style() if options.widget is None else options.widget.style()
+
+        doc = QTextDocument()
+        doc.setHtml(options.text)
+
+        options.text = ""
+        style.drawControl(QStyle.CE_ItemViewItem, options, painter)
+
+        ctx = QAbstractTextDocumentLayout.PaintContext()
+        if option.state & QStyle.State_Selected:
+            ctx.palette.setColor(QPalette.Text, option.palette.color(QPalette.Active, QPalette.HighlightedText))
+        else:
+            ctx.palette.setColor(QPalette.Text, option.palette.color(QPalette.Active, QPalette.HighlightedText))
+
+
+        textRect = style.subElementRect(QStyle.SE_ItemViewItemText, options)
+        painter.save()
+        painter.translate(textRect.topLeft())
+        painter.setClipRect(textRect.translated(-textRect.topLeft()))
+        doc.documentLayout().draw(painter, ctx)
+
+        painter.restore()
+
+    def sizeHint(self, option, index):
+        """sizeHint has also to be reimplemented."""
+        options = QStyleOptionViewItem(option)
+        self.initStyleOption(options, index)
+
+        doc = QTextDocument()
+        doc.setHtml(options.text)
+        doc.setTextWidth(options.rect.width())
+        return QSize(doc.idealWidth(), doc.size().height())
