47512e46f514a4c4e8616aecb831f22054621924
Jurikay <jurek.baumann@gmail.com>
2019-05-22 22:19:53 +0200

    Add helper functions

--- app/helpers.py
+++ app/helpers.py
@@ -1,5 +1,7 @@
 import os, sys, math
-
+from datetime import datetime
+import dateparser
+import pytz
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -26,3 +28,28 @@ def round2precision(val, precision: int = 0, which: str = ''):
     if which.lower() == 'down':
         round_callback = math.floor
     return '{1:.{0}f}'.format(precision, round_callback(val) / 10 ** precision)
+
+
+def check_file_exists(file_path):
+    """Return true if a file exists at the given path."""
+    return os.path.isfile(resource_path(file_path))
+
+def date_to_milliseconds(date_str):
+    """Convert UTC date to milliseconds
+    If using offset strings add "UTC" to date string e.g. "now UTC", "11 hours ago UTC"
+    See dateparse docs for formats http://dateparser.readthedocs.io/en/latest/
+    :param date_str: date in readable format, i.e. "January 01, 2018", "11 hours ago UTC", "now UTC"
+    :type date_str: str
+
+    Taken from: https://github.com/sammchardy/python-binance/tree/master/examples
+    """
+    # get epoch value in UTC
+    epoch = datetime.utcfromtimestamp(0).replace(tzinfo=pytz.utc)
+    # parse our date string
+    d = dateparser.parse(date_str)
+    # if the date is not timezone aware apply UTC timezone
+    if d.tzinfo is None or d.tzinfo.utcoffset(d) is None:
+        d = d.replace(tzinfo=pytz.utc)
+
+    # return the difference in time
+    return int((d - epoch).total_seconds() * 1000.0)
